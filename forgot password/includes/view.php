f007f06e55cc19a5f5c400f3d85695830cc69f79
Phathu <maddangerchris@gmail.com>
2025-09-23 10:53:37 +0200

    Delete forgot password directory

--- forgot password/includes/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-// sign in/includes/view.php (view)
-// Only responsible for rendering messages (no DB logic)
-
-if (!empty($message)) {
-    echo '<p style="color:red;">' . htmlspecialchars($message) . '</p>';
-}
-?>
